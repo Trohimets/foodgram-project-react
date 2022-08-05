bdc71342d752e7dc620960673da487b3089ef01c
Константин Трохимец <katrohimets@mail.ru>
2022-08-05 13:58:41 +0300

    start server 14

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -96,8 +96,8 @@ DATABASES = {
     }
 }
 
-Password validation
-https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+#Password validation
+#https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
    {
